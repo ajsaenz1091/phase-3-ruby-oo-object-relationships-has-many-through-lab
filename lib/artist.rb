b40877ade9fce933d9f5815d76d895de7c54806b
Alberto <ajsaenz1091@gmail.com>
2021-07-05 13:58:48 -0400

    Completed Assignment

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,30 @@
+class Artist
 
+    attr_reader :name
+
+    @@all = []
+
+    def initialize(name)
+        @name= name
+        @@all << self
+    end
+
+    def  self.all
+        @@all
+    end
+
+    # returns all songs associated with this Artist
+    def songs
+        Song.all.filter{|song| song.artist == self}
+    end
+
+    # given a name and genre, creates a new song associated with that artist
+    def new_song(name, genre)
+        song = Song.new(name, self, genre)
+    end
+
+    def genres
+        genres = self.songs.map(&:genre)
+    end
+
+end
